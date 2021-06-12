6b86c4568a1d95d28f54174ff700670e3d1e4006
William Farnsworth <williamcoreyf@gmail.com>
2021-06-11 22:19:10 -0400

    required route files

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`);
